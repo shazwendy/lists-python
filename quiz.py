fb749af1cfb1e25b6af676675700beb425cc316d
shazwendy <akothsharon19@gmail.com>
2019-05-07 19:07:45 +0300

    lists in python

---
+++ quiz.py
@@ -0,0 +1,60 @@
+def tens(numbers,list)
+	a=[]
+	for number in numbers:
+		numbers=[1,2,3,4,5,6,7,8,9]
+		q=number*10
+		a.append(q)
+		return a
+
+def squares(twos,list):
+	s = []
+	for x in twos:
+		twos=[2,4,6,8,10,12]
+		q=x*x
+		a.append(s)
+	return s
+
+def sorted(a,b):
+	a=[9,1,4,7,3]
+	b=[5,2,6,8,0]
+	r='a' + 'b'
+	sorted.sort(r)
+	print(r)
+
+def range_sum(number,n):
+	for x in range(1,n):
+		e=sum(1,n)
+		print(e)
+
+def function(largest,integers):
+	largest=[1,2,9,4,5,8,3]
+	print(largest[2])
+
+def smallest(list,integers):
+	smallest=[1,3,6,8,2,4,5,7]
+	print(smallest[0])
+
+def my_modulus(range,list):
+	t=[]
+	b=[]
+	for g in list:
+		if q%integer==0:
+			t.append(g)
+		else:
+			b.append(g)
+			print(t)
+			print(b)
+list = range(10,20)
+my_modulus(list,3)
+
+def students(name,balance):
+	student1={'balance':1000,'name':'Irene'}
+	student2={'balance':2000,'name':'Pauline'}
+	student3={'balance':3000,'name':'Naima'}
+	student4={'balance':1000,'name':'Rose'}
+	for student in students:
+	message="hello {}, your balance is {}".format(student["name"], student["balance"])
+	print(message)
+
+
+
